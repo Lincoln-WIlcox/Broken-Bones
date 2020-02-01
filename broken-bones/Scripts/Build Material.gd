16199cd5a9c135af71d63fc04fb532299e12b714
Jerimiah Gazlay <jgazlay@lyft.com>
2020-02-01 14:35:08 -0600

    fix drag and drop bones

--- broken-bones/Scripts/Build Material.gd
+++ broken-bones/Scripts/Build Material.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
 var lifted = false
+var mouse_pos
 
 func _process(delta):
+	mouse_pos = get_viewport().get_mouse_position()
 	if lifted:
-		self.position = get_viewport().get_mouse_position()
+		self.position = mouse_pos
 
 func _input(event):
    # Mouse in viewport coordinates
 	if event is InputEventMouseButton and event.pressed and event:
-		lifted = !lifted
-		print(event.as_text())
+		if match_mouse_pos():
+			lifted = !lifted
+
+
+func match_mouse_pos():
+	return mouse_pos.x > self.position.x - 20  and mouse_pos.x < self.position.x + 20 and mouse_pos.y > self.position.y - 20 and mouse_pos.y < self.position.y + 20
