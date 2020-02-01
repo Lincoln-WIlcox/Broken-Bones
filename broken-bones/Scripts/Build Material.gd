e776b458164692593d7488dbb2ba2e68fd83eafa
Jerimiah Gazlay <jgazlay@lyft.com>
2020-02-01 13:50:18 -0600

    add script to move bones

--- broken-bones/Scripts/Build Material.gd
+++ broken-bones/Scripts/Build Material.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
-var mouse_over = false
-
-func _on_Build_Material_mouse_entered():
-	mouse_over = true
-	print("mousing over")
-
-func _on_Build_Material_mouse_exited():
-	mouse_over = false
+var lifted = false
 
 func _process(delta):
-	print(mouse_over)
+	if lifted:
+		self.position = get_viewport().get_mouse_position()
+
+func _input(event):
+   # Mouse in viewport coordinates
+	if event is InputEventMouseButton and event.pressed and event:
+		lifted = !lifted
+		print(event.as_text())
