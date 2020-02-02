42a1812516cc6bd35bdb3106f85ee52b4b4d66ba
Lincoln Wilcox <lincolnpepper111@gmail.com>
2020-02-02 10:27:37 -0800

    Set window position to 0,0 so that the window doesn't start offscreen in builds

--- broken-bones/Scripts/Global.gd
+++ broken-bones/Scripts/Global.gd
@@ -2,6 +2,11 @@ extends Node
 
 var dinosaurs = []
 
+#to make sure window is placed correctly on build
+func _ready():
+	OS.set_window_position(Vector2())
+
+
 func add_dinosaur(dinosaur):
 	if dinosaurs.size() >= 3:
 		dinosaurs.remove(0)
