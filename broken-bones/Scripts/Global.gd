5a8b8ed12157edb1876e76637a558b723f542a3a
Jerimiah Gazlay <jgazlay@lyft.com>
2020-02-02 14:41:59 -0600

    use new assets in randomizer

--- broken-bones/Scripts/Global.gd
+++ broken-bones/Scripts/Global.gd
@@ -3,10 +3,37 @@ extends Node
 var dinosaurs = []
 
 var build_materials = [
-"res://Scenes/Build Materials/Garbage/8 Ball.tscn","res://Scenes/Build Materials/Garbage/Beer Bottle.tscn","res://Scenes/Build Materials/Garbage/Googly Eye.tscn","res://Scenes/Build Materials/Garbage/Nameplate.tscn","res://Scenes/Build Materials/Garbage/Propeller.tscn","res://Scenes/Build Materials/Garbage/Scissors.tscn","res://Scenes/Build Materials/Garbage/Stapler Remover.tscn","res://Scenes/Build Materials/Garbage/Toiler Paper.tscn",
-"res://Scenes/Build Materials/Mosasaurus/Spiny Arms.tscn","res://Scenes/Build Materials/Mosasaurus/Spiny Head.tscn","res://Scenes/Build Materials/Mosasaurus/Spiny Legs.tscn","res://Scenes/Build Materials/Mosasaurus/Spiny Neck.tscn","res://Scenes/Build Materials/Mosasaurus/Spiny Ribs.tscn","res://Scenes/Build Materials/Mosasaurus/Spiny Tail.tscn",
-"res://Scenes/Build Materials/Seabuddy/Seabuddy Arms.tscn","res://Scenes/Build Materials/Seabuddy/Seabuddy Head.tscn","res://Scenes/Build Materials/Seabuddy/Seabuddy Legs.tscn","res://Scenes/Build Materials/Seabuddy/Seabuddy Neck.tscn","res://Scenes/Build Materials/Seabuddy/Seabuddy Ribs.tscn","res://Scenes/Build Materials/Seabuddy/Seabuddy Tail.tscn",
-"res://Scenes/Build Materials/Trex/Trex Arms.tscn","res://Scenes/Build Materials/Trex/Trex Head.tscn","res://Scenes/Build Materials/Trex/Trex Legs.tscn","res://Scenes/Build Materials/Trex/Trex Neck.tscn","res://Scenes/Build Materials/Trex/Trex Ribs.tscn","res://Scenes/Build Materials/Trex/Trex Tail.tscn"
+"res://Scenes/Build Materials/Garbage/8 Ball.tscn",
+"res://Scenes/Build Materials/Garbage/Beer Bottle.tscn",
+"res://Scenes/Build Materials/Garbage/Googly Eye.tscn",
+"res://Scenes/Build Materials/Garbage/Nameplate.tscn",
+"res://Scenes/Build Materials/Garbage/Propeller.tscn",
+"res://Scenes/Build Materials/Garbage/Scissors.tscn",
+"res://Scenes/Build Materials/Garbage/Stapler Remover.tscn",
+"res://Scenes/Build Materials/Garbage/Toiler Paper.tscn",
+"res://Scenes/Build Materials/Mosasaurus/Spiny Arms.tscn",
+"res://Scenes/Build Materials/Mosasaurus/Spiny Head.tscn",
+"res://Scenes/Build Materials/Mosasaurus/Spiny Legs.tscn",
+"res://Scenes/Build Materials/Mosasaurus/Spiny Neck.tscn",
+"res://Scenes/Build Materials/Mosasaurus/Spiny Ribs.tscn",
+"res://Scenes/Build Materials/Mosasaurus/Spiny Tail.tscn",
+"res://Scenes/Build Materials/Seabuddy/Seabuddy Arms.tscn",
+"res://Scenes/Build Materials/Seabuddy/Seabuddy Head.tscn",
+"res://Scenes/Build Materials/Seabuddy/Seabuddy Legs.tscn",
+"res://Scenes/Build Materials/Seabuddy/Seabuddy Neck.tscn",
+"res://Scenes/Build Materials/Seabuddy/Seabuddy Ribs.tscn",
+"res://Scenes/Build Materials/Seabuddy/Seabuddy Tail.tscn",
+"res://Scenes/Build Materials/Trex/Trex Arms.tscn",
+"res://Scenes/Build Materials/Trex/Trex Head.tscn",
+"res://Scenes/Build Materials/Trex/Trex Legs.tscn",
+"res://Scenes/Build Materials/Trex/Trex Neck.tscn",
+"res://Scenes/Build Materials/Trex/Trex Ribs.tscn",
+"res://Scenes/Build Materials/Trex/Trex Tail.tscn",
+"res://Scenes/Build Materials/Garbage/3D Glasses.tscn",
+"res://Scenes/Build Materials/Garbage/Alien.tscn",
+"res://Scenes/Build Materials/Garbage/Dragonball.tscn",
+"res://Scenes/Build Materials/Garbage/Robot Arm.tscn",
+"res://Scenes/Build Materials/Garbage/Swordfish.tscn",
 ]
 
 var timer: Timer
