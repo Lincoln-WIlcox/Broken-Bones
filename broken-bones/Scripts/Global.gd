f1abfb54c1d226b02e6d709e9b8e04ddaf3bd1f2
Jerimiah Gazlay <jgazlay@lyft.com>
2020-02-02 12:08:19 -0600

    add global timer

--- broken-bones/Scripts/Global.gd
+++ broken-bones/Scripts/Global.gd
@@ -2,15 +2,26 @@ extends Node
 
 var dinosaurs = []
 
-var storetimer = 0
+var timer: Timer
+var puzzle_time := 10
 
 #to make sure window is placed correctly on build
 func _ready():
 	OS.set_window_position(Vector2())
+	
+	timer = Timer.new()
+	timer.connect("timeout",self,"_on_timer_timeout")
+	timer.set_one_shot(true)
+	add_child(timer)
+	timer.start(puzzle_time)
 
 
 func add_dinosaur(dinosaur):
 	if dinosaurs.size() >= 3:
 		dinosaurs.remove(0)
 	dinosaurs.append(dinosaur)
+
+
+func _on_timer_timeout():
+	get_tree().change_scene("res://Scenes/Game Over Screen.tscn")
 	
--- broken-bones/Scripts/Timer.gd
+++ broken-bones/Scripts/Timer.gd
@@ -1,21 +1,7 @@
 extends ProgressBar
 
-var timer: Timer
-export var puzzle_time: int
 
-func _ready() -> void:
-	timer = Timer.new()
-	timer.connect("timeout",self,"_on_timer_timeout")
-	timer.set_one_shot(true)
-	add_child(timer)
-	timer.start(puzzle_time)
-	
-	
 func _process(delta: float) -> void:
-	var percentage = (timer.time_left / puzzle_time) * 100
+	var percentage = (Global.timer.time_left / Global.puzzle_time) * 100
 	self.value = percentage
-
-
-func _on_timer_timeout():
-	get_tree().change_scene("res://Scenes/Game Over Screen.tscn")
  
