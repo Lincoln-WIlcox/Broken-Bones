87befdfb70d5c723c133e42517aaa8ca47199dfd
Lincoln Wilcox <lincolnpepper111@gmail.com>
2020-02-08 18:55:49 -0800

    scaled down dinos in trophy room and changed size of dino box and where dino parts can spawn

--- broken-bones/Scripts/Dinosaur Parts.gd
+++ broken-bones/Scripts/Dinosaur Parts.gd
@@ -5,7 +5,7 @@ export var random_parts_amount: int
 func _ready():
 	for i in range(random_parts_amount):
 		var part = Global.build_materials[rand_range(0,Global.build_materials.size())]
-		var randomposition = Vector2(rand_range(1413, 1830),rand_range(10,1000))
+		var randomposition = Vector2(rand_range(1500, 1830),rand_range(10,1000))
 		part = load(part).instance()
 		part.position = randomposition
-		add_child(part)
+		add_child(part) 
