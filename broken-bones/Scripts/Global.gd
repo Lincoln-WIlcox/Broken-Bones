315772bc9e27110ee22e6e6e91cb6079173299e3
Jerimiah Gazlay <jgazlay@lyft.com>
2020-02-23 17:18:12 -0600

    pull files from folder and add new assets

--- broken-bones/Scripts/Global.gd
+++ broken-bones/Scripts/Global.gd
@@ -1,52 +1,15 @@
 extends Node
 
+const VOICE_LINE_PATH = "res://Assets/Sound/Voice Lines/Submission/"
+const GARBAGE_MATERIAL_PATH = "res://Scenes/Build Materials/Garbage/"
+const DINO_MATERIAL_PATH = "res://Scenes/Build Materials/Dinos/"
+
 var dinosaurs = []
 
-var build_materials = [
-"res://Scenes/Build Materials/Garbage/8 Ball.tscn",
-"res://Scenes/Build Materials/Garbage/Beer Bottle.tscn",
-"res://Scenes/Build Materials/Garbage/Googly Eye.tscn",
-"res://Scenes/Build Materials/Garbage/Nameplate.tscn",
-"res://Scenes/Build Materials/Garbage/Propeller.tscn",
-"res://Scenes/Build Materials/Garbage/Scissors.tscn",
-"res://Scenes/Build Materials/Garbage/Stapler Remover.tscn",
-"res://Scenes/Build Materials/Garbage/Toiler Paper.tscn",
-"res://Scenes/Build Materials/Mosasaurus/Spiny Arms.tscn",
-"res://Scenes/Build Materials/Mosasaurus/Spiny Head.tscn",
-"res://Scenes/Build Materials/Mosasaurus/Spiny Legs.tscn",
-"res://Scenes/Build Materials/Mosasaurus/Spiny Neck.tscn",
-"res://Scenes/Build Materials/Mosasaurus/Spiny Ribs.tscn",
-"res://Scenes/Build Materials/Mosasaurus/Spiny Tail.tscn",
-"res://Scenes/Build Materials/Seabuddy/Seabuddy Arms.tscn",
-"res://Scenes/Build Materials/Seabuddy/Seabuddy Head.tscn",
-"res://Scenes/Build Materials/Seabuddy/Seabuddy Legs.tscn",
-"res://Scenes/Build Materials/Seabuddy/Seabuddy Neck.tscn",
-"res://Scenes/Build Materials/Seabuddy/Seabuddy Ribs.tscn",
-"res://Scenes/Build Materials/Seabuddy/Seabuddy Tail.tscn",
-"res://Scenes/Build Materials/Trex/Trex Arms.tscn",
-"res://Scenes/Build Materials/Trex/Trex Head.tscn",
-"res://Scenes/Build Materials/Trex/Trex Legs.tscn",
-"res://Scenes/Build Materials/Trex/Trex Neck.tscn",
-"res://Scenes/Build Materials/Trex/Trex Ribs.tscn",
-"res://Scenes/Build Materials/Trex/Trex Tail.tscn",
-"res://Scenes/Build Materials/Garbage/3D Glasses.tscn",
-"res://Scenes/Build Materials/Garbage/Alien.tscn",
-"res://Scenes/Build Materials/Garbage/Dragonball.tscn",
-"res://Scenes/Build Materials/Garbage/Robot Arm.tscn",
-"res://Scenes/Build Materials/Garbage/Swordfish.tscn",
-"res://Scenes/Build Materials/Garbage/ok.tscn",
-"res://Scenes/Build Materials/Garbage/Lightsaber.tscn",
-"res://Scenes/Build Materials/Garbage/Alien2.tscn",
-]
-
-var voicelines = [
-"res://Assets/Sound/Dino-Mite 1.wav",
-"res://Assets/Sound/Now Thats A Discount Dino 1.wav",
-"res://Assets/Sound/Now Thats A Discount Dino 2.wav",
-"res://Assets/Sound/Sticks n Stones.wav",
-"res://Assets/Sound/Yawn thats a good dino.wav",
-"res://Assets/Sound/Eating.wav",
-]
+onready var garbage_materials = get_files(GARBAGE_MATERIAL_PATH)
+onready var dino_bones = get_files(DINO_MATERIAL_PATH)
+onready var build_materials = garbage_materials + dino_bones
+onready var voicelines = get_voice_audio_files()
 
 var timer: Timer
 var puzzle_time := 80
@@ -54,14 +17,13 @@ var puzzle_time := 80
 #to make sure window is placed correctly on build
 func _ready():
 	randomize()
-	
+
 	timer = Timer.new()
 	timer.connect("timeout",self,"_on_timer_timeout")
 	timer.set_one_shot(true)
 	add_child(timer)
-	
+
 	OS.set_window_position(Vector2())
-	
 
 
 func add_dinosaur(dinosaur):
@@ -69,14 +31,53 @@ func add_dinosaur(dinosaur):
 		dinosaurs.remove(0)
 	dinosaurs.append(dinosaur)
 
+
 func _on_timer_timeout():
 	get_tree().change_scene("res://Scenes/Game Over Screen.tscn")
-	
+
 
 func play_voiceline():
 	Music.get_node("Dino-Mite").stream = load(voicelines[rand_range(0,voicelines.size())])
 	Music.get_node("Dino-Mite").play()
 
+
 func start_game():
 	get_tree().change_scene("res://Scenes/Levels/TRex Level.tscn")
 	timer.start(puzzle_time)
+
+
+func get_voice_audio_files() -> Array:
+	"""
+	gets all files in the voice lines folder and filter out non audio files
+	"""
+
+	var voice_line_files = get_files(VOICE_LINE_PATH)
+	var voice_audio_files = []
+
+	for file in voice_line_files:
+		if file.get_extension() == "wav":
+			voice_audio_files.append(file)
+
+	return voice_audio_files
+
+
+func get_files(root_path: String) -> Array:
+	var files = []
+	var dir = Directory.new()
+
+	if dir.open(root_path) == OK:
+		dir.list_dir_begin(true, false)
+
+		var file_name = dir.get_next()
+
+		while (file_name != ""):
+			var path = dir.get_current_dir() + "/" + file_name
+
+			if dir.current_is_dir():
+				files = files + get_files(path)
+			else:
+				files.append(path)
+
+			file_name = dir.get_next()
+
+	return files
