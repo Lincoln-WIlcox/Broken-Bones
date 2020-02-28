d027d58c9c3abc8eb2145e91f326c94f5916b201
Jerimiah Windows <jdgazlay@gmail.com>
2020-02-27 23:44:02 -0600

    add controls for bones

--- broken-bones/Broken Bones Concepts.md
+++ broken-bones/Broken Bones Concepts.md
@@ -9,10 +9,10 @@ You work for a museum with low standards, and your job is to glue together dinos
 1. Name dinosaurs - `Done`
 1. you make exhibits - `Done`
 	- 3 dinos are an exhibit
-1. Timer limits how long you have before you fail - `Not Started`
+1. Timer limits how long you have before you fail - `Done`
 	1. Timer doesn't start until you drag first piece - `Not Started`
 	1. Progress bar type timer - `Done`
-	1. When timer runs out, you lose your dinosaur bones and get no points - `Not Started`
+	1. When timer runs out, you lose your dinosaur bones and get no points - `Done (minus points)`
 
 ### Ideas:
 - Points
@@ -20,3 +20,7 @@ You work for a museum with low standards, and your job is to glue together dinos
 - Pedestals in background with your previous creations
 - Drone flies bones away
 - you are rated on those exhibits
+- horizontally flip items - `WIP`
+- rotate - `WIP`
+- scale items
+- change z index on items (no futher than the background) - `WIP`
--- broken-bones/Scripts/Build Material.gd
+++ broken-bones/Scripts/Build Material.gd
@@ -3,21 +3,59 @@ extends Area2D
 var lifted := false
 var mouse_pos: Vector2
 export var selection_variance := Vector2(20, 20)
-onready var original_scale = get_scale()
+onready var current_scale = get_scale()
 
 func _process(delta: float) -> void:
-	mouse_pos = get_viewport().get_mouse_position()
-	if lifted:
-		position = mouse_pos
-		set_scale(original_scale + Vector2(.05, .05))
-	else:
-		set_scale(original_scale)
+	handle_item_update()
+	handle_moving()
+
 
 func _input(event):
 	# Mouse in viewport coordinates
 	if event and event is InputEventMouseButton and event.pressed:
 		if match_mouse_pos():
 			lifted = !lifted
+
+
+func handle_item_update() -> void:
+	if lifted:
+		update_rotation()
+		update_z_index()
+		update_scale()
+		update_h_flip()
+
+
+func update_rotation() -> void:
+	# based on Input we'll return rotation degrees needed to change
+	set_rotation_degrees(rotation_degrees + (Input.get_action_strength("Rotate Right") - Input.get_action_strength("Rotate Left")) * 5)
+
+
+func update_z_index() -> void:
+	if Input.is_action_just_pressed("Push Foreground"):
+		set_z_index(clamp(z_index - 1, 1, 100))
+	if Input.is_action_just_pressed("Push Background"):
+		set_z_index(clamp(z_index + 1, 1, 100))
+
+
+func update_scale() -> void:
+	var scale_direction = (Input.get_action_strength("Scale Up") - Input.get_action_strength("Scale Down")) / 50
+	current_scale += Vector2(scale_direction, scale_direction)
+
+
+func update_h_flip() -> void:
+	if Input.is_action_just_pressed("Flip"):
+		var sprite = get_node("Sprite")
+		sprite.set_flip_h(!sprite.flip_h)
+
+
+func handle_moving() -> void:
+	mouse_pos = get_viewport().get_mouse_position()
+
+	if lifted:
+		position = mouse_pos
+		set_scale(current_scale + Vector2(.05, .05))
+	else:
+		set_scale(current_scale)
 
 
 func match_mouse_pos():
